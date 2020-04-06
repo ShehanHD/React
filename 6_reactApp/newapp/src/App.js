2e6d1999fbac3b4e611b55c374f46e3cd8134130
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2020-04-06 13:21:34 +0200

    le prove

---
+++ 6_reactApp/newapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Test from './test';
+
+function App() {
+  return (
+    <div className="App">
+      <p>ciao</p>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
---
+++ 6_reactApp/newapp/src/test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div className="test">
+      <p>name : shenan</p>
+      <p>surname : HD</p>
+    </div>
+  );
+}
+
+export default Test;
