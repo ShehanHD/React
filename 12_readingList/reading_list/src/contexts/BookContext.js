e6570d64085eef3ff5fd1c3b7342ae4f8bbbe8cc
ShehanHD <51677228+ShehanHD@users.noreply.github.com>
2020-04-19 13:08:52 +0200

    add reducer

--- 12_readingList/reading_list/src/contexts/BookContext.js
+++ 12_readingList/reading_list/src/contexts/BookContext.js
@@ -1,24 +1,13 @@
-import React, { createContext, useState } from 'react';
-import { v1 as uuid } from 'uuid';
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducer/bookReduser';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBook] = useState([
-        { title: "ciao1", author: "mondo1", id: 1},
-        { title: "ciao2", author: "mondo2", id: 2},
-        { title: "ciao3", author: "mondo3", id: 3}
-    ]);
+    const [books, dispatch] = useReducer(bookReducer, []);
 
-    const addBook = (title, author) => {
-        setBook([...books, { title, author, id: uuid() }]);
-    }
-
-    const removeBook = (id) => {
-        setBook(books.filter(item => item.id !== id)); //Quando il condizione è vera agg. al array 
-    }
     return ( 
-        <BookContext.Provider value={{ books, addBook, removeBook }}>
+        <BookContext.Provider value={{ books, dispatch }}>
             { props.children }
         </BookContext.Provider>
     );
